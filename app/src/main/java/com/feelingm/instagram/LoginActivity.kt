de867638d6995069903feccf7f487b03e4e78e77
Hyunwoo <feelingm@live.com>
2018-11-24 17:08:06 +0900

    commit

--- app/src/main/java/com/feelingm/instagram/LoginActivity.kt
+++ app/src/main/java/com/feelingm/instagram/LoginActivity.kt
@@ -2,8 +2,8 @@ package com.feelingm.instagram
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import com.facebook.AccessToken
@@ -18,10 +18,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.*
-import com.twitter.sdk.android.core.Callback
-import com.twitter.sdk.android.core.Result
-import com.twitter.sdk.android.core.TwitterException
-import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.*
 import com.twitter.sdk.android.core.identity.TwitterAuthClient
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -33,9 +30,9 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var callbackManager: CallbackManager
 
-    lateinit var twitterAuthClient: TwitterAuthClient
+    val GOOGLE_LOGIN_CODE = 901
 
-    val GOOGLE_LOGIN_CODE = 9000
+    lateinit var twitterAuthClient: TwitterAuthClient
 
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +47,7 @@ class LoginActivity : AppCompatActivity() {
                 .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        callbackManager = CallbackManager.Factory.create()
 
         google_sign_in_button.setOnClickListener { googleLogin() }
 
@@ -58,11 +56,12 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener { emailLogin() }
 
         twitter_sign_in_button.setOnClickListener { twitterLogin() }
+
     }
 
     fun moveMainPage(user: FirebaseUser?) {
         user?.let {
-            Toast.makeText(this, getString(R.string.signin_complete), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.signin_complete, Toast.LENGTH_SHORT).show()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
@@ -71,7 +70,6 @@ class LoginActivity : AppCompatActivity() {
     @SuppressLint("RestrictedApi")
     private fun googleLogin() {
         progress_bar.visibility = View.VISIBLE
-
         startActivityForResult(googleSignInClient.signInIntent, GOOGLE_LOGIN_CODE)
     }
 
@@ -80,10 +78,9 @@ class LoginActivity : AppCompatActivity() {
 
         LoginManager.getInstance().let {
             it.logInWithReadPermissions(this, mutableListOf("public_profile", "email"))
-            it.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+            it.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult?) {
                     handleFacebookAccessToken(result?.accessToken)
-
                 }
 
                 override fun onCancel() {
@@ -97,40 +94,40 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun twitterLogin() {
-        progress_bar.visibility = View.VISIBLE
-        twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
-            override fun success(result: Result<TwitterSession>?) {
-                val credential = TwitterAuthProvider.getCredential(
-                        result?.data?.authToken?.token!!,
-                        result.data?.authToken?.secret!!)
-                auth.signInWithCredential(credential).addOnCompleteListener { task ->
+    fun createAndLoginEmail() {
+        auth.createUserWithEmailAndPassword(email_edittext.text.toString(),
+                password_edittext.text.toString())
+                .addOnCompleteListener { task ->
                     progress_bar.visibility = View.GONE
                     if (task.isSuccessful) {
+                        Toast.makeText(this, R.string.signup_complete, Toast.LENGTH_SHORT).show()
                         moveMainPage(auth.currentUser)
+                    } else if (task.exception?.message.isNullOrEmpty()) {
+                        Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                    } else {
+                        signInEmail()
                     }
+
                 }
-            }
-
-            override fun failure(exception: TwitterException?) {
-
-            }
-        })
     }
 
-    private fun handleFacebookAccessToken(accessToken: AccessToken?) {
-        val credential = FacebookAuthProvider.getCredential(accessToken!!.token)
-        auth.signInWithCredential(credential).addOnCompleteListener { task ->
-            progress_bar.visibility = View.GONE
-            if (task.isSuccessful) {
-                moveMainPage(auth.currentUser)
-            }
-        }
+    private fun signInEmail() {
+        auth.signInWithEmailAndPassword(email_edittext.text.toString(),
+                password_edittext.text.toString())
+                .addOnCompleteListener { task ->
+                    progress_bar.visibility = View.GONE
+
+                    if (task.isSuccessful) {
+                        moveMainPage(auth.currentUser)
+                    } else {
+                        Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 
     private fun emailLogin() {
         if (email_edittext.text.toString().isEmpty() ||
-                password_edittext.text.toString().isEmpty()) {
+                        password_edittext.text.toString().isEmpty()) {
             Toast.makeText(this, R.string.signout_fail_null, Toast.LENGTH_SHORT).show()
         } else {
             progress_bar.visibility = View.VISIBLE
@@ -138,28 +135,32 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("StringFormatInvalid")
-    private fun createAndLoginEmail() {
-        auth.createUserWithEmailAndPassword(email_edittext.text.toString(),
-                password_edittext.text.toString()).addOnCompleteListener { task ->
-            progress_bar.visibility = View.GONE
+    private fun twitterLogin() {
+        progress_bar.visibility = View.VISIBLE
+        twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>?) {
 
-            when {
-                task.isSuccessful -> {
-                    Toast.makeText(this, getString(R.string.signup_complete, 0), Toast.LENGTH_LONG).show()
-                    moveMainPage(auth.currentUser)
+                val credential = TwitterAuthProvider.getCredential(
+                        result?.data?.authToken?.token!!,
+                        result.data?.authToken?.secret!!)
+
+                auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                    progress_bar.visibility = View.GONE
+
+                    if (task.isSuccessful) {
+                        moveMainPage(auth.currentUser)
+                    }
                 }
-                task.exception?.message.isNullOrEmpty() -> Toast.makeText(this, task.exception!!.message, Toast.LENGTH_LONG).show()
-                else -> signInEmail()
             }
 
-        }
+            override fun failure(exception: TwitterException?) {
+            }
+        })
     }
 
-    private fun signInEmail() {
-        auth.signInWithEmailAndPassword(email_edittext.text.toString(),
-                password_edittext.text.toString()).addOnCompleteListener { task ->
-
+    private fun handleFacebookAccessToken(accessToken: AccessToken?) {
+        val credential = FacebookAuthProvider.getCredential(accessToken?.token!!)
+        auth.signInWithCredential(credential).addOnCompleteListener { task ->
             progress_bar.visibility = View.GONE
 
             if (task.isSuccessful) {
@@ -169,7 +170,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
         callbackManager.onActivityResult(requestCode, resultCode, data)
 
@@ -178,17 +178,20 @@ class LoginActivity : AppCompatActivity() {
         if (requestCode == GOOGLE_LOGIN_CODE) {
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result.isSuccess) {
-                firebaseAuthWithGoogle(result.signInAccount)
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
             } else {
                 progress_bar.visibility = View.GONE
-            }
-        }
+            } }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
         auth.signInWithCredential(credential).addOnCompleteListener { task ->
             progress_bar.visibility = View.GONE
+
             if (task.isSuccessful) {
                 moveMainPage(auth.currentUser)
             }
